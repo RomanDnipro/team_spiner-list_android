a4d21514a04a95a454232d20db0a644733d0c88c
RomanDnipro <bogdanovroman1221@gmail.com>
2017-04-30 01:31:00 +0300

    first commit

---
+++ app/src/main/java/com/example/dev/teamlist/GroupListActivity.java
@@ -0,0 +1,15 @@
+package com.example.dev.teamlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GroupListActivity extends Activity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_list);
+    }
+}
